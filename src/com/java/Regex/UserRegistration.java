7a9deb05f4c8bece7995aa20d3a146752faeaf90
am22ar <amarpatil12222@gmail.com>
2022-10-27 13:36:08 +0530

    Added: Regex program for Validating First Name Logic

--- src/com/java/Regex/UserRegistration.java
+++ src/com/java/Regex/UserRegistration.java
@@ -1,7 +1,22 @@
 package com.java.Regex;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+	static boolean isValid(String regex, String userInput)
+	{
+		return Pattern.compile(regex).matcher(userInput).matches();
+	}
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to Java Regex Program");
+		String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+		System.out.println("Enter User FirstName:");
+		String firstName = sc.next();
+		System.out.println(isValid(firstNamePattern, firstName));
+
+		
+
 	}
 }
